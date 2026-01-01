8f3f5d3acf90b65d0d7e7aa802fe2582fc4eca64
eRaquet <git.far@raquetfamily.com>
2026-01-01 11:51:11 -0500

    refactor: switch board class away from gym.Env and RLlib

--- modules/chess_types.py
+++ modules/chess_types.py
@@ -1,6 +1,6 @@
 """File for containing all the type aliases and structures used in SmartChessV2."""
 
-from enum import Enum
+from enum import Enum, IntFlag, auto
 from typing import (
     TypedDict,
 )
@@ -29,8 +29,7 @@ BOARD_ENCODING_SHAPE = (8, 8, 18)
 # an set of some number of board encodings, to be kept together (i.e. game trajectories or observations)
 type SetEncoding = NDArray[np.uint8]  # has a shape of (n, 8, 8, 18)
 type Observation = SetEncoding
-type IsOver = bool
-type MoveReward = float
+type Trajectory = SetEncoding
 
 # alias for gym.Env terminology
 # corresponds to the index of the chosen move
@@ -44,6 +43,18 @@ class DisplayMode(Enum):
     GUI = 1
     ASCII = 2
     NONE = 3
+
+
+class BoardOutcome(IntFlag):
+    """Enumeration of various states of outcome for a chess board."""
+
+    UNDECIDED = auto()
+    WHITE = auto()
+    BLACK = auto()
+    DRAW = auto()
+
+    TERMINATED = WHITE | BLACK | DRAW
+    WON = WHITE | BLACK
 
 
 # dictionary structure to specify the info read off from the board at each position
