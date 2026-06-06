e332abe6d5e0fab632662749528516e4d80cb248
eRaquet <git.far@raquetfamily.com>
2026-06-06 11:39:46 -0400

    add `STRAIN_COUNT` configuration to `config.py`

--- demos/model_demos/seed_model_strains.py
+++ demos/model_demos/seed_model_strains.py
@@ -3,24 +3,15 @@
 import json
 from pathlib import Path
 
-from modules.config import PROJECT_PATH
+from modules.config import PROJECT_PATH, STRAIN_COUNT
 from modules.model import StandardModel
 
 if __name__ == "__main__":
     # define metadata to track model generations
-    metadata = {
-        "strain_0_curr_gen": 0,
-        "strain_1_curr_gen": 0,
-        "strain_2_curr_gen": 0,
-        "strain_3_curr_gen": 0,
-        "strain_4_curr_gen": 0,
-        "strain_5_curr_gen": 0,
-        "strain_6_curr_gen": 0,
-        "strain_7_curr_gen": 0,
-    }
+    metadata = {}
+    for i in range(STRAIN_COUNT):
+        metadata[f"strain_{i}_curr_gen"] = 0
+        StandardModel(i, 0, construct=True)
 
     with Path.open(PROJECT_PATH / "data" / "saved_models" / "metadata.json", "w") as file:
         json.dump(metadata, file)
-
-    for i in range(8):
-        StandardModel(i, 0, construct=True)
--- modules/config.py
+++ modules/config.py
@@ -2,4 +2,8 @@
 
 from pathlib import Path
 
+# path object to project directory
 PROJECT_PATH = Path(__file__).parent.parent
+
+# number of model strains
+STRAIN_COUNT = 4
